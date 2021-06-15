f6d76b0424291ddd312d6c2bd0f6ca8e4913a560
Chris Watts <chris.watts.t@gmail.com>
2021-06-15 13:10:49 -0700

    chore(package): add dist for link

---
+++ dist/types/utils/transform.utils.d.ts
@@ -0,0 +1,5 @@
+export declare const dateToDateTimeString: (date: Date) => string;
+export declare const dateToDateString: (date: Date | string) => string;
+export declare const dateToTimeString: (date: Date | string) => string;
+export declare const simpleArrayToString: (value: any[] | any) => string[] | any;
+export declare const stringToSimpleArray: (value: string | any) => any[];
